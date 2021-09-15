45cb58d7e55c46e64ced2b52fdb468f35e2d2139
haraGADygyl <ti60gtt@gmail.com>
2021-09-15 22:04:39 +0300

    JS Advanced

---
+++ JS_Advanced/02-syntax-functions-and-statements-exercise/04-previous-day.js
@@ -0,0 +1,10 @@
+function previousDay(year, month, day) {
+
+    let dateString = year + '-' + month + '-' + day;
+    let event = new Date(dateString);
+    event.setDate(day - 1);
+    console.log(event.getFullYear() + '-' + Number(event.getMonth() + 1) + '-' + event.getDate());
+}
+
+previousDay(2016, 9, 30);
+previousDay(2016, 10, 1);
