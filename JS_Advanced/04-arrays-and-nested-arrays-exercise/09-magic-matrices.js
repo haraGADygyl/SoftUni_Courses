cf298fff061dac59d7fe944826b3e5362512504e
haraGADygyl <ti60gtt@gmail.com>
2021-09-22 20:15:44 +0300

    JS Advanced

---
+++ JS_Advanced/04-arrays-and-nested-arrays-exercise/09-magic-matrices.js
@@ -0,0 +1,20 @@
+function magicMatrices(params) {
+
+    
+    
+}
+
+magicMatrices([[4, 5, 6],
+                [6, 5, 4],
+                [5, 5, 5]]
+   );
+
+magicMatrices([[11, 32, 45],
+                [21, 0, 1],
+                [21, 1, 1]]
+   );
+
+magicMatrices([[1, 0, 0],
+                [0, 0, 1],
+                [0, 1, 0]]
+   );
