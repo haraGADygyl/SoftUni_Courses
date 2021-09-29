cb6cf91a92f51b72843ed85735f362e96971882a
haraGADygyl <ti60gtt@gmail.com>
2021-09-29 18:53:57 +0300

    JS Advanced

--- JS_Advanced/08-DOM-introduction-exercise/04. Search in List/search.js
+++ JS_Advanced/08-DOM-introduction-exercise/04. Search in List/search.js
@@ -1,3 +1,24 @@
 function search() {
-   // TODO
+
+   let towns = Array.from(document.querySelectorAll('ul li'));
+   let text = document.getElementById('searchText').value;
+   let countMatches = 0;
+
+
+   towns.forEach((el) => {
+
+      if (el.textContent.includes(text)) {
+
+         el.style.fontWeight = 'bold';
+         el.style.textDecoration = 'underline';
+         countMatches++;
+
+      } else {
+         el.style.fontWeight = 'normal';
+         el.style.textDecoration = '';
+      }
+   })
+
+   document.getElementById('result').textContent = `${countMatches} matches found`;
+
 }
--- JS_Advanced/08-DOM-introduction-exercise/05. Table - Search Engine/solution.js
+++ JS_Advanced/08-DOM-introduction-exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,20 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let input = document.getElementById('searchField')
+      let inputText = input.value.toLowerCase();
+      let tableELements = Array.from(document.querySelectorAll('tbody tr'));
 
+      tableELements.forEach((el) => {
+         let text = el.textContent.toLowerCase();
+
+         if (text.includes(inputText)) {
+            el.classList.add('select');
+         } else {
+            el.classList.remove('select');
+         }
+      })
+
+      input.value = '';
    }
 }
