53325c74db46e51997bb1f44560234e862be9c45
haraGADygyl <ti60gtt@gmail.com>
2021-09-30 11:16:16 +0300

    JS Advanced

--- JS_Advanced/08-DOM-introduction-exercise/06. Format the Text/solution.js
+++ JS_Advanced/08-DOM-introduction-exercise/06. Format the Text/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO
+
+  let text = document.getElementById('input').value;
+  let sentences = text.split('.');
+  sentences.pop();
+  let result = '';
+
+  for (let i = 0; i < sentences.length; i += 3) {
+
+    result += `<p>${sentences[i]}.`;
+
+    if (sentences[i + 1] != undefined) {
+      result += sentences[i + 1] + '.';
+    }
+
+    if (sentences[i + 2] != undefined) {
+      result += sentences[i + 2] + '.';
+    }
+
+    result += '</p>'
+  }
+
+  document.getElementById('output').innerHTML = result;
 }
