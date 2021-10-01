7138f5bc8fa8d4ae910cba3e33ff7c6516de2b41
haraGADygyl <ti60gtt@gmail.com>
2021-10-01 21:10:19 +0300

    JS Advanced

---
+++ JS_Advanced/10-DOM-manipulations-and-events-exercise/01. Sections/script.js
@@ -0,0 +1,20 @@
+function create(words) {
+   let content = document.getElementById('content');
+
+   for (let word of words) {
+      let div = document.createElement('div');
+      let paragraph = document.createElement('p');
+
+      paragraph.textContent = word;
+      paragraph.style.display = 'none';
+
+      div.appendChild(paragraph);
+      div.addEventListener('click', reveal);
+
+      content.appendChild(div);
+
+      function reveal(event) {
+         event.currentTarget.children[0].style.display = '';
+      }
+   }
+}
