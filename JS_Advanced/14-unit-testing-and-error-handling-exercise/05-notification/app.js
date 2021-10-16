18a0e13bd8a2eceb3adfb14ab97340ea513e8f68
haraGADygyl <ti60gtt@gmail.com>
2021-10-16 18:39:41 +0300

    JS Advanced

--- JS_Advanced/14-unit-testing-and-error-handling-exercise/05-notification/app.js
+++ JS_Advanced/14-unit-testing-and-error-handling-exercise/05-notification/app.js
@@ -1,4 +1,17 @@
 function notify(message) {
-  console.log('todo')
-  // TODO:
+    let notificationDiv = document.querySelector('#notification');
+    notificationDiv.textContent = message;
+    notificationDiv.style.display = 'block';
+
+    let dataNotificationAttribute = notificationDiv.getAttribute('data-notification-set');
+
+    if (dataNotificationAttribute !== 'true') {
+        notificationDiv.setAttribute('data-notification-set', 'true');
+        notificationDiv.addEventListener('click', notificationDivClickHandler);
+    }
+
+    function notificationDivClickHandler(event) {
+        let notificationDiv = document.querySelector('#notification');
+        notificationDiv.style.display = 'none';
+    }
 }
