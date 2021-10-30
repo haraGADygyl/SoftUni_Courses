16ec8ae838e6d6e831b99acd6dbae46d59818fea
haraGADygyl <ti60gtt@gmail.com>
2021-10-30 22:37:54 +0300

    JS Application

--- JS_Applications/05-asynchronous-programming/02.Github-Repos/scripts/app.js
+++ JS_Applications/05-asynchronous-programming/02.Github-Repos/scripts/app.js
@@ -1,3 +1,32 @@
 function loadRepos() {
-	console.log("TODO...");
+    const username = document.getElementById('username').value;
+    const url = `https://api.github.com/users/${username}/repos`;
+    const list = document.getElementById('repos');
+
+    fetch(url)
+        .then(response => {
+            if (response.ok === false) {
+                throw new Error(`${response.status} ${response.statusText}`)
+            }
+            return response.json()
+        })
+        .then(handleResponse)
+        .catch(handleError)
+
+    function handleResponse(data) {
+        list.innerHTML = '';
+
+        for (const repo of data) {
+            const liElement = document.createElement('li');
+            liElement.innerHTML = `<a href="${repo.html_url}">
+    ${repo.full_name}
+</a>`
+            list.appendChild(liElement)
+        }
+    }
+
+    function handleError(error) {
+        list.innerHTML = '';
+        list.textContent = `${error.message}`
+    }
 }
