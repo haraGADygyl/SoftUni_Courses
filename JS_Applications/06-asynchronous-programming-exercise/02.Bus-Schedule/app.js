1bf386fb15845f136ff49c4c0dec768ef583ac5f
haraGADygyl <ti60gtt@gmail.com>
2021-11-01 19:23:59 +0200

    JS Application

--- JS_Applications/06-asynchronous-programming-exercise/02.Bus-Schedule/app.js
+++ JS_Applications/06-asynchronous-programming-exercise/02.Bus-Schedule/app.js
@@ -1,15 +1,38 @@
 function solve() {
 
+    const infoElement = document.querySelector('#info');
+    const departBtn = document.querySelector('#depart');
+    const arriveBtn = document.querySelector('#arrive');
 
-    function depart() {
+    let stop = {
+        next: 'depot'
+    }
 
+    async function depart() {
 
+        try {
+            departBtn.disabled = true;
+            const url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
 
+            const res = await fetch(url);
+           stop = await res.json();
 
+            infoElement.textContent = `Next stop ${stop.name}`;
+
+            arriveBtn.disabled = false;
+        } catch (error) {
+            infoElement.textContent = 'Error';
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+        }
     }
 
     function arrive() {
 
+        infoElement.textContent = `Arriving at ${stop.name}`;
+
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
--- JS_Applications/06-asynchronous-programming-exercise/06.Blog/app.js
+++ JS_Applications/06-asynchronous-programming-exercise/06.Blog/app.js
@@ -1,5 +1,53 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.querySelector('#btnLoadPosts').addEventListener('click', getAllPosts);
+    document.querySelector('#btnViewPost').addEventListener('click', displayPosts);
 }
 
 attachEvents();
+
+function displayPosts() {
+
+
+    
+}
+
+async function getAllPosts() {
+    const url = `http://localhost:3030/jsonstore/blog/posts`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    const selectElement = document.querySelector('#posts');
+    selectElement.textContent = '';
+    Object.values(data).forEach(p => {
+        const optionElement = document.createElement('option');
+        optionElement.textContent = p.title;
+        optionElement.value = p.id;
+
+        selectElement.appendChild(optionElement);
+    })
+}
+
+async function getPostById(postId) {
+
+    const url = `http://localhost:3030/jsonstore/blog/posts/` + postId;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data;
+
+}
+
+async function getComments(postId) {
+
+    const url = `http://localhost:3030/jsonstore/blog/comments`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    const comments = Object.values(data).filter(c => c.postId === postId)
+
+    return comments;
+
+}
