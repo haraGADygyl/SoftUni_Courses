8db69dc8f87bc483744884ff1cd0ac58eafc7595
haraGADygyl <ti60gtt@gmail.com>
2021-11-02 22:50:10 +0200

    Flask

--- JS_Applications/06-asynchronous-programming-exercise/06.Blog/app.js
+++ JS_Applications/06-asynchronous-programming-exercise/06.Blog/app.js
@@ -5,10 +5,31 @@ function attachEvents() {
 
 attachEvents();
 
-function displayPosts() {
+async function displayPosts() {
 
+    const postTitle = document.getElementById('post-title');
+    const postBody = document.getElementById('post-body');
+    const ulElement = document.getElementById('post-comments');
 
-    
+    postTitle.textContent = 'Loading...';
+    postBody.textContent = '';
+    ulElement.textContent = '';
+
+    const selectedId = document.getElementById('posts').value;
+
+    const [post, comments] = await Promise.all([
+        getPostById(selectedId),
+        getComments(selectedId)
+    ])
+
+    postTitle.textContent = post.title;
+    postBody.textContent = post.body;
+
+    comments.forEach(c => {
+        const liElement = document.createElement('li');
+        liElement.textContent = c.text;
+        ulElement.appendChild(liElement);
+    });
 }
 
 async function getAllPosts() {
@@ -33,10 +54,7 @@ async function getPostById(postId) {
     const url = `http://localhost:3030/jsonstore/blog/posts/` + postId;
 
     const res = await fetch(url);
-    const data = await res.json();
-
-    return data;
-
+    return await res.json();
 }
 
 async function getComments(postId) {
@@ -46,8 +64,5 @@ async function getComments(postId) {
     const res = await fetch(url);
     const data = await res.json();
 
-    const comments = Object.values(data).filter(c => c.postId === postId)
-
-    return comments;
-
+    return Object.values(data).filter(c => c.postId === postId);
 }
--- JS_Applications/08-remote-data-and-authentication-exercise/02.Phonebook/app.js
+++ JS_Applications/08-remote-data-and-authentication-exercise/02.Phonebook/app.js
@@ -1,5 +1,80 @@
 function attachEvents() {
-    console.log('TODO...');
+
+    const loadBtn = document.getElementById('btnLoad');
+    const createBtn = document.getElementById('btnCreate');
+
+
+    loadBtn.addEventListener('click', loadContacts)
+    createBtn.addEventListener('click', onCreate)
+
+    phonebookElement.addEventListener('click', onDelete)
+
+    loadContacts()
+
 }
 
+const phonebookElement = document.getElementById('phonebook');
+const personElement = document.getElementById('person');
+const phoneElement = document.getElementById('phone');
+
 attachEvents();
+
+async function onDelete(event) {
+    const id = event.target.dataset.id;
+    if (id !== undefined) {
+        await deleteContact(id);
+        event.target.parentElement.remove()
+    }
+
+}
+
+async function onCreate() {
+    const person = personElement.value;
+    const phone = phoneElement.value;
+    const contact = {person, phone}
+
+    const result = await createContact(contact);
+
+    phonebookElement.appendChild(createItem(result));
+
+}
+
+async function loadContacts() {
+
+    const res = await fetch('http://localhost:3030/jsonstore/phonebook');
+    const data = await res.json();
+
+    phonebookElement.textContent = ''
+
+    Object.values(data).map(createItem).forEach(i => phonebookElement.appendChild(i))
+
+}
+
+function createItem(contact) {
+    const liElement = document.createElement('li');
+    liElement.innerHTML = `${contact.person}: ${contact.phone} <button data-id="${contact._id}">Delete</button>`
+
+    return liElement;
+}
+
+async function createContact(contact) {
+    const res = await fetch('http://localhost:3030/jsonstore/phonebook', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(contact)
+    });
+    const result = await res.json();
+
+    return result;
+}
+
+async function deleteContact(id) {
+    const res = await fetch('http://localhost:3030/jsonstore/phonebook/' + id, {
+        method: 'delete',
+    });
+    const result = await res.json();
+
+    return result;
+}
