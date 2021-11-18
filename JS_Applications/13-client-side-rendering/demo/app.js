071410e644f98ab02a267a85652a5d8d48e63a29
haraGADygyl <ti60gtt@gmail.com>
2021-11-18 09:21:08 +0200

    JS Application

--- JS_Applications/13-client-side-rendering/demo/app.js
+++ JS_Applications/13-client-side-rendering/demo/app.js
@@ -1,9 +1,10 @@
-import {html, render} from 'https://unpkg.com/lit-html?module';
+import {html, render} from '../node_modules/lit-html/lit-html.js';
 
 const articleTemplate = (data) => html`
     <article>
+        <input type="text" ?disabled=${data.disabled}/>
         <h3>${data.title}</h3>
-        <div class="content-body">
+        <div class=${data.color}>
             <p>${data.content}</p>
         </div>
         <footer>Author: ${data.author}</footer>
@@ -17,10 +18,24 @@ async function start() {
     const renderBtn = document.querySelector('#renderBtn')
     renderBtn.addEventListener('click', onRender)
 
+    document.getElementById('changeBtn').addEventListener('click', onChange)
+
     function onRender() {
         data[0].author += 1
         const result = data.map(articleTemplate)
         render(result, main)
+    }
+
+    function onChange() {
+        data.shift()
+
+        data.unshift({
+            "title": "First Article 2",
+            "content": "Test na Jorko",
+            "author": "Jorko Jorkov"
+        })
+
+        onRender()
     }
 }
 
