9cb1f094cdc7d40d05644e672a9ab3ad62fa736c
haraGADygyl <ti60gtt@gmail.com>
2021-11-20 13:51:32 +0200

    JS Application

--- JS_Applications/14-client-side-rendering-exercise/03.Search-in-List/search.js
+++ JS_Applications/14-client-side-rendering-exercise/03.Search-in-List/search.js
@@ -1,3 +1,37 @@
-function search() {
-   // TODO
+import {html, render} from './node_modules/lit-html/lit-html.js'
+import {towns as townNames} from "./towns.js";
+
+const listTemplate = (towns) => html`
+    <ul>
+        ${towns.map(town => html`
+            <li class=${town.match ? 'active' : ''}>${town.name}</li>`)}
+    </ul>`
+
+const towns = townNames.map(town => ({name: town, match: false}))
+
+const root = document.querySelector('#towns')
+const input = document.querySelector('#searchText')
+const output = document.querySelector('#result')
+document.querySelector('button').addEventListener('click', onSearch)
+
+update()
+
+function update() {
+    render(listTemplate(towns), root)
+}
+
+function onSearch() {
+    const match = input.value.trim().toLocaleLowerCase()
+    let matches = 0
+    for (let town of towns) {
+        if (match && town.name.toLocaleLowerCase().includes(match)) {
+            town.match = true
+            matches++
+        } else {
+            town.match = false
+        }
+    }
+
+    output.textContent = `${matches} matches found`
+    update()
 }
--- JS_Applications/14-client-side-rendering-exercise/04.Fill-Dropdown/dropdown.js
+++ JS_Applications/14-client-side-rendering-exercise/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,41 @@
-function addItem() {
-    console.log('TODO:...');
+import {html, render} from './node_modules/lit-html/lit-html.js'
+
+const selectTemplate = (items) => html`
+    <select id="menu">
+        ${items.map(item => html`
+            <option value=${item._id}>${item.text}</option>`)}
+    </select>`
+
+const url = 'http://localhost:3030/jsonstore/advanced/dropdown'
+const div = document.querySelector('div')
+document.querySelector('form').addEventListener('submit', addItem)
+
+getData()
+
+async function getData() {
+    const res = await fetch(url)
+    const data = await res.json()
+    update(Object.values(data))
+}
+
+function update(items) {
+    const result = selectTemplate(items)
+    render(result, div)
+}
+
+async function addItem(event) {
+    event.preventDefault()
+    const text = document.querySelector('#itemText').value
+
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({text})
+    })
+
+    if (res.ok) {
+        await getData()
+    }
 }
