23465e03cd0496de62ff2c18d0c13b774b7b5ce0
haraGADygyl <ti60gtt@gmail.com>
2021-11-28 17:29:09 +0200

    JS Advanced

--- JS_Applications/20-exam-prep/src/views/details.js
+++ JS_Applications/20-exam-prep/src/views/details.js
@@ -11,17 +11,6 @@ const detailsTemplate = (book, isOwner, onDelete) => html`
             <div class="actions">
                 ${isOwner ? html`<a class="button" href="/edit/${book._id}">Edit</a>
                 <a @click=${onDelete} class="button" href="javascript:void(0)">Delete</a>` : null}
-
-<!--                &lt;!&ndash; Bonus &ndash;&gt;-->
-<!--                &lt;!&ndash; Like button ( Only for logged-in users, which is not creators of the current book ) &ndash;&gt;-->
-<!--                <a class="button" href="#">Like</a>-->
-
-<!--                &lt;!&ndash; ( for Guests and Users )  &ndash;&gt;-->
-<!--                <div class="likes">-->
-<!--                    <img class="hearts" src="/images/heart.png">-->
-<!--                    <span id="total-likes">Likes: 0</span>-->
-<!--                </div>-->
-<!--                &lt;!&ndash; Bonus &ndash;&gt;-->
             </div>
         </div>
         <div class="book-description">
@@ -30,12 +19,9 @@ const detailsTemplate = (book, isOwner, onDelete) => html`
         </div>
     </section>`
 
-
 export async function detailsPage(ctx) {
     const book = await getBookById(ctx.params.id)
-
     const userData = getUserData()
-
     const isOwner = userData && book._ownerId === userData.id
 
     ctx.render(detailsTemplate(book, isOwner, onDelete))
