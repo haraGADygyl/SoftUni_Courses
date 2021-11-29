1aa1e552fc36a346b568ab44952c44ad415efb69
haraGADygyl <ti60gtt@gmail.com>
2021-11-29 18:04:13 +0200

    JS Advanced

---
+++ JS_Applications/21-test-exam/Meme_Lounge/src/app.js
@@ -0,0 +1,51 @@
+import * as api from './api/data.js'
+window.api = api
+
+import {page, render} from './lib.js'
+import {homePage} from "./views/home.js";
+import {catalogPage} from "./views/catalog.js";
+import {loginPage} from "./views/login.js";
+import {registerPage} from "./views/register.js";
+import {logout} from "./api/data.js";
+import {getUserData} from "./util.js";
+import {createPage} from "./views/create.js";
+import {detailsPage} from "./views/details.js";
+
+const root = document.querySelector('main')
+document.querySelector('#logoutBtn').addEventListener('click', onLogout)
+
+page(decorateContext)
+page('/', homePage)
+page('/memes', catalogPage)
+page('/login', loginPage)
+page('/register', registerPage)
+page('/create', createPage)
+page('/details/:id', detailsPage)
+
+updateUserNav()
+page.start()
+
+function decorateContext(ctx, next) {
+    ctx.render = (content) => render(content, root)
+    ctx.updateUserNav = updateUserNav
+    next()
+}
+
+function onLogout() {
+    logout()
+    updateUserNav()
+    page.redirect('/')
+}
+
+function updateUserNav() {
+    const userData = getUserData()
+
+    if (userData) {
+        document.querySelector('.user').style.display = 'block'
+        document.querySelector('.guest').style.display = 'none'
+        document.querySelector('.user span').textContent = `Welcome, ${userData.email}`
+    } else {
+        document.querySelector('.user').style.display = 'none'
+        document.querySelector('.guest').style.display = 'block'
+    }
+}
