13c305f24b8adb5a5cd29c760d4f983cddb63939
haraGADygyl <ti60gtt@gmail.com>
2021-12-08 23:04:15 +0200

    Flask

--- Web_Applications_with_Flask/main.py
+++ Web_Applications_with_Flask/main.py
@@ -1,12 +1,22 @@
 from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
+
+books = ['a', 'b', 'c']
 
 
-@app.route('/')
-def hello():
-    return 'Hello Jorko'
+class Books(Resource):
+    def get(self):
+        return books
 
+    def post(self):
+        books.append('d')
+        return books
+
+
+api.add_resource(Books, '/books')
 
 if __name__ == '__main__':
     app.run()
