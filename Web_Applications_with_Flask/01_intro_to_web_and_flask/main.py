4a6dfbaf7237b3f0330c1d48b3f37bd264796b53
haraGADygyl <ti60gtt@gmail.com>
2021-12-11 20:09:08 +0200

    Flask

--- Web_Applications_with_Flask/01_intro_to_web_and_flask/main.py
+++ Web_Applications_with_Flask/01_intro_to_web_and_flask/main.py
@@ -1,22 +1,28 @@
-from flask import Flask
-from flask_restful import Api, Resource
-
-app = Flask(__name__)
-api = Api(app)
-
-books = ['a', 'b', 'c']
-
-
-class Books(Resource):
-    def get(self):
-        return books
-
-    def post(self):
-        books.append('d')
-        return books
-
-
-api.add_resource(Books, '/books')
-
-if __name__ == '__main__':
-    app.run()
+# from flask import Flask, request
+# from flask_restful import Resource, Api
+# from flask_sqlalchemy import SQLAlchemy
+#
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:manushev@localhost:5433/store'
+# db = SQLAlchemy(app)
+# api = Api(app)
+#
+#
+# class BookModel(db.Model):
+#     __tablename__ = 'books'
+#
+#     pk = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(255), nullable=False)
+#     author = db.Column(db.String(255), nullable=False)
+#
+#
+# class Books(Resource):
+#     def post(self):
+#         data = request.data
+#
+#
+# api.add_resource(Books, '/books')
+#
+#
+# if __name__ == '__main__':
+#     app.run()
