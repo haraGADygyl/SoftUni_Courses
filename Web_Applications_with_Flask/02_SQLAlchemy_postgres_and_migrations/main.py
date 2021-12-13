1d04b6f5e2fd24defe72029ca8a3357deefae38f
haraGADygyl <ti60gtt@gmail.com>
2021-12-13 21:39:15 +0200

    Flask

--- Web_Applications_with_Flask/02_SQLAlchemy_postgres_and_migrations/main.py
+++ Web_Applications_with_Flask/02_SQLAlchemy_postgres_and_migrations/main.py
@@ -2,9 +2,11 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from decouple import config
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:manushev@localhost:5433/store'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{config("DB_USER")}:{config("DB_PASSWORD")}' \
+                                        f'@localhost:{config("DB_PORT")}/{config("DB_NAME")}'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 api = Api(app)
@@ -17,6 +19,7 @@ class BookModel(db.Model):
     title = db.Column(db.String(255), nullable=False)
     author = db.Column(db.String(255), nullable=False)
     reader_pk = db.Column(db.Integer, db.ForeignKey('readers.pk'))
+    reader = db.relationship('ReaderModel')
 
     def __str__(self):
         return f'pk: {self.pk} Title: {self.title} Author: {self.author}'
@@ -31,6 +34,7 @@ class ReaderModel(db.Model):
     pk = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(255), nullable=False)
     last_name = db.Column(db.String(255), nullable=False)
+    books = db.relationship('BookModel', backref='book', lazy='dynamic')
 
     def serialize(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
@@ -48,8 +52,14 @@ class Books(Resource):
         db.session.commit()
         return {'message': 'created'}, 201
 
+class Readers(Resource):
+    def get(self):
+        readers = ReaderModel.query.all()
+        return {'readers': [r.serialize() for r in readers]}
+
 
 api.add_resource(Books, '/books')
+api.add_resource(Readers, '/readers')
 db.create_all()
 
 if __name__ == '__main__':
