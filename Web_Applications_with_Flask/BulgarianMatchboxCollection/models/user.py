f8a15a91c778ac8632b85be6a070e76bff1764ce
haraGADygyl <ti60gtt@gmail.com>
2022-01-01 22:58:26 +0200

    Added user and administrator models

---
+++ Web_Applications_with_Flask/BulgarianMatchboxCollection/models/user.py
@@ -0,0 +1,24 @@
+from models.enums import RoleType
+from db import db
+
+
+class BaseUserModel(db.Model):
+    __abstract__ = True
+
+    pk = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(255), nullable=False)
+    last_name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+
+
+class UserModel(BaseUserModel):
+    __tablename__ = "users"
+
+    role = db.Column(db.Enum(RoleType), default=RoleType.user, nullable=False)
+
+
+class AdministratorModel(BaseUserModel):
+    __tablename__ = "administrators"
+
+    role = db.Column(db.Enum(RoleType), default=RoleType.administrator, nullable=False)
